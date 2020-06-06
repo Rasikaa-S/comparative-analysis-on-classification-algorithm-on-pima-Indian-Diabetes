969ec0f1b79d217c8f88e7ed1ee5db65ba145339
Rasikaa-S <55550065+Rasikaa-S@users.noreply.github.com>
2020-06-06 13:44:27 +0530

    Add files via upload

---
+++ classification.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+df = pd.read_csv (r'C:\Users\Lenovo\Downloads\diabetes.csv')
+print (df)
+
+X_train, X_test, y_train, y_test = train_test_split(df.loc[:, df.columns != 'Outcome'], df['Outcome'], stratify=df['Outcome'], random_state=50)
+tree = DecisionTreeClassifier(random_state=0)
+tree.fit(X_train, y_train)
+print("Decision tree classifier:")
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+
+print("Random forest classifier:")
+rf = RandomForestClassifier(n_estimators=100, random_state=0)
+rf.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(rf.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(rf.score(X_test, y_test)))
+
+print("Support Vector Machine")
+svc = SVC()
+svc.fit(X_train, y_train)
+print("Accuracy on training set: {:.2f}".format(svc.score(X_train, y_train)))
+print("Accuracy on test set: {:.2f}".format(svc.score(X_test, y_test)))
+
+print("Logistic Regression")
+logreg = LogisticRegression().fit(X_train, y_train)
+logreg001 = LogisticRegression(C=100).fit(X_train, y_train)
+print("Training set accuracy: {:.3f}".format(logreg001.score(X_train, y_train)))
+print("Test set accuracy: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+print("K Nearest Neighbour")
+knn = KNeighborsClassifier(n_neighbors=9)
+knn.fit(X_train, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'.format(knn.score(X_train, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'.format(knn.score(X_test, y_test)))
+
+
